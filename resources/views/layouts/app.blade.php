9a0d22800b91910818c47388c5e0ecfe052f9d48
michael de groote <michael.degroote@obasi.be>
2020-08-31 13:38:07 +0000

    CHANGED: andere kleur voor de top balk voor TEST omgeving om duidelijk te maken dat die een TEST omgeving is

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -156,7 +156,7 @@
         @endif
         @endauth
 
-        <v-app-bar app color="indigo" dark>
+        <v-app-bar app color="orange" dark>
             <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
             <v-toolbar-title>TADD Calc</v-toolbar-title>
         </v-app-bar>
